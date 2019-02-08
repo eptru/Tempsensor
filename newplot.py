8e75d7262d6062ea06c9866d8ef7f2795a3f74e5
eptru <eptru@hotmail.com>
2019-02-08 14:59:37 -0500

    better!

--- newplot.py
+++ newplot.py
@@ -11,11 +11,14 @@ import itertools
 
 filename=strftime("%Y%m", localtime())+'TemperatureLogFile.csv'
 currentmonth = localtime().tm_mon
+linesshowing=500000 #50000 lines is around 20 hrs.
+step=1 #how many steps you want to take between two points on graph?
 
-data=[]
-name=[]
-names={}
-linenow=0 #current line number of file
+data=[] #data from csv file
+names={} #sensor names
+linenow=0 #which line am I currently reading
+
+#readin sensor list and names from sensorList.dat file
 with open("sensorList.dat", 'r') as sensorfile:
 	spamreader = csv.reader(sensorfile)
 	for row in spamreader:
@@ -27,6 +30,7 @@ with open("sensorList.dat", 'r') as sensorfile:
 print ("Sensors:\n",names)
 namehere = []
 
+#initially read in csv file
 with open(filename, 'r') as csvfile:
 	spamreader = csv.reader(csvfile)
 	for row in spamreader:
@@ -40,16 +44,16 @@ for i in range(2, len(dataArray)):
 		print ("Sensors:\n",names)
 
 	namehere.append(names[dataArray[i,0]])
-dataArray=np.delete(dataArray,(0),1)
-dataArray=np.delete(dataArray,(1),0)
+dataArray=np.delete(dataArray,(0),1) #delete header line from data
+dataArray=np.delete(dataArray,(1),0) #delete second column, the datetime one
 
 
-dataArray[dataArray==''] = 0
-dataArray = dataArray.astype(np.float)
-beginningtime=dataArray[0,0]
+dataArray[dataArray==''] = 0 #no data set temp to zero. Change if actual temperature could be zero. 
+dataArray = dataArray.astype(np.float) #convert everything to float
+beginningtime=dataArray[0,0] #the t=0 point!
 
 dataArray[0] -= beginningtime
-dataArray[0] /=3600
+dataArray[0] /=3600 #unit: s
 
 class TestClass(QtGui.QMainWindow):
 	def __init__(self):
@@ -62,7 +66,7 @@ class TestClass(QtGui.QMainWindow):
 		win = QtGui.QWidget()
 		self.plt = pg.PlotWidget(name='Plot1')  ## giving the plots names allows us to link their axes together
 		self.plt.showGrid(x=True,y=True)
-		self.plt.setLabel('left', "Temp", units='C')
+		self.plt.setLabel('left', "Temp", units='C') 
 		self.plt.setLabel('bottom', "time", units='h')
 		self.setCentralWidget(win)
 		self.timer = pg.QtCore.QTimer()
@@ -75,23 +79,23 @@ class TestClass(QtGui.QMainWindow):
 		self.curve.scene().sigMouseMoved.connect(self.mouseMoved)
 		#step=linenow//10000
 		plots = []
-		self.plt.setYRange(2, 60, padding=0)
-		for q in range(1,len(dataArray)):
-			plots.append(self.plt.plot(dataArray[0, -50000::][dataArray[q, -50000::]>0] ,dataArray[q, -50000::][dataArray[q, -50000::]>0], pen=(q,9), name = namehere[q-1]))
+		#self.plt.setYRange(2, 60, padding=0)
+		for q in range(1,len(dataArray)): #the first graph! ignores all temperature data that is less then and equal to zero. Change if your temperature data can actually reach zero.
+			plots.append(self.plt.plot(dataArray[0, -linesshowing::step][dataArray[q, -linesshowing::step]>0] ,dataArray[q, -linesshowing::step][dataArray[q, -linesshowing::step]>0], pen=(q,9), name = namehere[q-1]))
 		
 		def update() :
-			if (localtime().tm_mon!=currentmonth):
+			if (localtime().tm_mon!=currentmonth): #restart the program every month since the csv file renews every month! Not sure if this is working though.
 				os.execl(sys.executable, sys.executable, *sys.argv)
-			global dataArray
+			global dataArray 
 			global linenow
-			global maxlen
+			global maxlen #the maximum length of the temperature data per line.
 			#self.plt.clear()
 			csvfile=open(filename)
 			# try:
 				# self.plt.legend.scene().removeItem(self.plt.legend)
 			# except Exception as e:
 				# print (e)
-			for i, line in enumerate(csvfile):
+			for i, line in enumerate(csvfile): #update data
 				if i<=linenow:
 					continue
 				linenow=i
@@ -102,12 +106,13 @@ class TestClass(QtGui.QMainWindow):
 					for i in range(len(dataArray)+1, len(dummy)):
 						arr=np.zeros(len(dataArray[0]), float)
 						dataArray=np.append(dataArray, [arr], axis=0)
-						if not dummyy[i] in names:
-							names[dummyy[i]] = dummyy[i][-4:]
+						if not dummyy[i] in names: #if new temperature added
+							names[dummyy[i]] = dummyy[i][-4:] #add new sensor name
 							print ("Sensors:\n",names)
 
 						namehere.append(names[dummyy[i]])
-						plots.append(self.plt.plot(dataArray[0, -50000::][dataArray[q, -50000::]>0], dataArray[i, -50000::][dataArray[q, -50000::]>0], pen=(i,9), name=namehere[i-1]))
+						#plot new sensor graph
+						plots.append(self.plt.plot(dataArray[0, -linesshowing::step][dataArray[q, -linesshowing::step]>0] ,dataArray[q, -linesshowing::step][dataArray[q, -linesshowing::step]>0], pen=(i,9), name=namehere[i-1]))
 				arr=np.zeros((len(dataArray), 1), float)
 				for o in range(len(dummy)):
 					if (o==0):
@@ -116,19 +121,20 @@ class TestClass(QtGui.QMainWindow):
 					if (o==1):
 						continue
 					if (dummy[o]==''):
-						arr[o-1,0]=0
+						arr[o-1,0]=0 #if no data then set to zero. Change if blah blah
 						continue
 					arr[o-1,0] = float(dummy[o])
 				dataArray=np.append(dataArray, arr, axis=1)
 			#step=linenow//10000
-			for q in range(1,len(dataArray)):
-				plots[q-1].setData(dataArray[0, -50000::][dataArray[q, -50000::]>0],dataArray[q, -50000::][dataArray[q, -50000::]>0])
+			for q in range(1,len(dataArray)): #update plot data
+				plots[q-1].setData(dataArray[0, -linesshowing::step][dataArray[q, -linesshowing::step]>0] ,dataArray[q, -linesshowing::step][dataArray[q, -linesshowing::step]>0])
 				
 		self.timer.timeout.connect(update)
-		self.timer.start(10)
+		self.timer.start(500) #unit: milliseconds
 		
 	def mouseMoved(self, evt):
 			p = self.plt.plotItem.vb.mapSceneToView(evt)
+			#the widget at the bottom
 			self.statusBar().showMessage("Time: {} Temp: {}".format(time.asctime(time.localtime(p.x()*3600+beginningtime)), p.y()))		
 ## Start Qt event loop.
 def main():
